73447c5795e4be90ff41cb1af7a93496853cd79b
sailesh-tamang <saileshtamang909@gmail.com>
2025-03-01 14:51:19 +0545

    backend

---
+++ backend/dbconfig.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+module.exports = {
+  "db": {
+  //   "database": process.env.database,
+  //   "username": process.env.username,
+  //   "password": process.env.password,
+  //  " port": process.env.port,
+  //   "host": process.env.host,
+  //   "dialect": process.env.dialect,
+
+  "database": "EccWork",
+  "username": "postgres",
+
+  "password": "freefire0852",
+  "host": "localhost",
+  "port": 5432,
+  "dialect": "postgres"
+  },
+};
